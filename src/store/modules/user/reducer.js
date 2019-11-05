ca67d46c889f29c34ea4962dc7da8f75e9e107e3
eduqg <eduardoqgomes@gmail.com>
2019-11-05 08:19:07 -0300

    Adding user data to store

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,18 +1,15 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  token: null,
-  signed: false,
-  loading: false,
+  profile: null,
 };
 
-export default function auth(state = INITIAL_STATE, action) {
+export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
     // No reducer de autenticação é armazena apenas o token e a informação se está logado ou não
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
-        draft.token = action.payload.token;
-        draft.signed = true;
+        draft.profile = action.payload.user;
       });
     default:
       return state;
